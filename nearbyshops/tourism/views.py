54b2d5f3d585720e239366cad1faf72caa1c23de
RioSakao <skoqush@gmail.com>
2023-11-10 14:24:42 -0800

    Applied Dynamic Yelp Search in Tourism/Views.py

--- nearbyshops/tourism/views.py
+++ nearbyshops/tourism/views.py
@@ -1,28 +1,26 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from django.contrib.gis.measure import D
-from django.contrib.gis.db.models import PointField
+
+# from . import my_hand
+from .my_hand import businesses_data
+from .my_hand import *
 
 from .models import Tourism
 from .serializers import TourismSerializer
 
+
 class TourismViews(APIView):
-    def get(self,request):
+    def get(self, request):
         TourismModel = Tourism.objects.all()
         serializer = TourismSerializer(TourismModel, many=True)
         return Response({"TourismModel": serializer.data})
 
-    def post(self,request,format=None):
+    def post(self, request, format=None):
         search_query = (request.data).split(",")
-        lat = int(search_query[0])
-        long = int(search_query[1])
-        base_point = PointField(lat,long)
-        tourism = Tourism.objects
-        #nearby queries within 150 km
-        matched_data = tourism.filter(
-            location__distance_lte=(
-                base_point, D(km=150))
-                ).distance(base_point).order_by('distance')
-        serializer = TourismSerializer(matched_data,many=True)    
+        lat = search_query[0]
+        long = search_query[1]
+        term = search_query[2]
+        Search(lat, long, term)
+        serializer = TourismSerializer(businesses_data, many=True)
         return Response(data=serializer.data)
