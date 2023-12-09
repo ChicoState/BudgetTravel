a72fe8f4ac2f1c67c5eeab9895c614f00f76cd09
RioSakao <skoqush@gmail.com>
2023-12-09 12:29:17 -0800

    Auth view updated

--- nearbyshops/auth/views.py
+++ nearbyshops/auth/views.py
@@ -16,6 +16,7 @@ from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
+from rest_framework import status
 
 
 @csrf_exempt  # Disable CSRF protection for this view
@@ -49,8 +50,26 @@ class MyObtainTokenPairView(TokenObtainPairView):
     permission_classes = (AllowAny,)
     serializer_class = MyTokenObtainPairSerializer
 
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            return Response(serializer.validated_data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+    @api_view(['POST'])
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return Response("Success")
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
