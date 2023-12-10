7390d43390a6343f0b00c6046b5550f997e11beb
DarrianMC <darrianmcallister@gmail.com>
2023-12-09 22:00:30 -0800

    update Dec 9

--- App/Screens/PlanTripScreen3.tsx
+++ App/Screens/PlanTripScreen3.tsx
@@ -1,13 +1,63 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import axios from 'axios';
 
 const PlanTripScreen3: React.FC<{ navigation: any }> = ({ navigation }) => {
   const [budget, setBudget] = useState('');
-  
-  const handleNext = () => {
+  let responseKeyValuePairs = {};
+  const handleNext = async () => {
+
+
+  const postData =  [
+  { lat: '39.7388', long: '-121.8500', term: 'Restaurants' },
+  { lat: '39.7388', long: '-121.8500', term: 'Arts & Entertainment' },
+  { lat: '39.7388', long: '-121.8500', term: 'Shopping' },
+  { lat: '39.7388', long: '-121.8500', term: 'Nightlife' },
+  { lat: '39.7388', long: '-121.8500', term: 'Hotels & Travel' },
+  { lat: '39.7388', long: '-121.8500', term: 'Beauty & Spas"' },
+  { lat: '39.7388', long: '-121.8500', term: 'Arts & Entertainment' },
+  { lat: '39.7388', long: '-121.8500', term: 'Arts & Entertainment' },
+  { lat: '39.7388', long: '-121.8500', term: 'Arts & Entertainment' },
+  { lat: '39.7388', long: '-121.8500', term: 'Arts & Entertainment' }
+];
+
+  try {
+    for (let i = 0; i < 10; i++) 
+    {
+      const response = await axios.post
+      (
+      	apiUrl, 
+      	postData[i], // The data to send
+      	{
+        		headers: {'Content-Type': 'application/json',},
+      	}
+      
+      );
+
+      // { "lat": "39.7388", "long": "-121.8500", "term": "Restaurants" } im assuming the response is in this form
+      const responseData = response.data;
+      
+
+      responseKeyValuePairs[`Response_${i + 1}`] = {
+        lat: responseData.lat,
+        long: responseData.long,
+        term: responseData.term,
+      };
+    }
+
+    // To display the response for debugging purposes
+    console.log('Response key-value pairs:', responseKeyValuePairs);
+  } catch (error) {
+    console.error('There was an error with the requests:', error);
+  }
+    
+
+    
     if (parseInt(budget) > 0) {
       navigation.navigate('TripDisplayMainPage', { budgetAmount: budget });
     }
+    
+     
   };
 
   return (
